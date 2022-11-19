576b2225883e27dc509fd8a8d430b915bcb6c78c
Abel Shields <abshield@cisco.com>
2022-11-19 16:15:16 +0000

    Remove unnecessary #[must_use]

--- src/adaptive_card.rs
+++ src/adaptive_card.rs
@@ -848,28 +848,24 @@ impl Column {
     }
 
     /// Adds element to column
-    #[must_use]
     pub fn add_element(&mut self, item: CardElement) -> Self {
         self.items.push(item);
         self.into()
     }
 
     /// Sets separator
-    #[must_use]
     pub fn set_separator(&mut self, s: bool) -> Self {
         self.separator = Some(s);
         self.into()
     }
 
     /// Sets `VerticalContentAlignment`
-    #[must_use]
     pub fn set_vertical_alignment(&mut self, s: VerticalContentAlignment) -> Self {
         self.vertical_content_alignment = Some(s);
         self.into()
     }
 
     /// Sets width
-    #[must_use]
     pub fn set_width<T: Into<String>>(&mut self, s: T) -> Self {
         self.width = Some(s.into());
         self.into()
